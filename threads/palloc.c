ef32324af9113d12457db9c860767d51b539133d
JJ Lau <j.j.j.lau@live.com>
2015-11-15 17:22:02 +0900

    Attempting to put in frame

--- threads/palloc.c
+++ threads/palloc.c
@@ -60,7 +60,9 @@ palloc_init (size_t user_page_limit)
   init_pool (&kernel_pool, free_start, kernel_pages, "kernel pool");
   init_pool (&user_pool, free_start + kernel_pages * PGSIZE,
              user_pages, "user pool");
+  printf ("BEFORE FRAME_TABLE ###############################################\n");
   init_frame_table (user_pages);
+  printf ("AFTER FRAME_TABLE ################################################\n");
 }
 
 /* Obtains and returns a group of PAGE_CNT contiguous free pages.
