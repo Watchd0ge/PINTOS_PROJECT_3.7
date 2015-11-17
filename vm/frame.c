8d7a9e824995ac4599851302875ab266139289a0
JJ Lau <j.j.j.lau@live.com>
2015-11-17 12:49:33 +0900

    Using palloc_get_page repeatedly

---
+++ vm/frame.c
@@ -0,0 +1,39 @@
+#include <hash.h>
+#include <list.h>
+#include "frame.h"
+// #include "swap.h"
+#include "userprog/pagedir.h"
+#include "threads/thread.h"
+#include "threads/pte.h"
+#include "threads/malloc.h"
+#include "threads/palloc.h"
+#include "threads/pte.h"
+#include "threads/init.h"
+
+#include <stdio.h>
+
+/* Supplemental Page Table is global. */
+struct hash sup_pt;
+struct lock sup_pt_lock;
+
+/* Frame Table */
+struct list frame_list;
+struct lock frame_list_lock;
+
+/* Lock for frame eviction */
+struct lock evict_lock;
+
+/* "Hand" in clock algorithm for frame eviction */
+struct frame_struct* evict_pointer;
+
+/* Initialize supplemental page table and frame table */
+void
+sup_pt_init (void)
+{
+  hash_init (&sup_pt, sup_pt_hash_func, sup_pt_less_func, NULL);
+  list_init (&frame_list);
+  lock_init (&sup_pt_lock);
+  lock_init (&frame_list_lock);
+  lock_init (&evict_lock);
+  evict_pointer = NULL;
+}
