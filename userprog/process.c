284c0ffeb71ec2a0749186e1bb20e8fa346d4c61
JJ Lau <j.j.j.lau@live.com>
2015-11-18 01:47:09 +0900

    -

--- userprog/process.c
+++ userprog/process.c
@@ -485,7 +485,7 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
       /* Add the page to the process's address space. */
       if (!install_page (upage, kpage, writable))
         {
-          deallocate_frame (kpage)
+          deallocate_frame (kpage);
           // palloc_free_page (kpage);
           return false;
         }
