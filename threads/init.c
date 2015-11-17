1fbf6fdc8d7aced9760b32f4f66c6e90010cfc7d
JJ Lau <j.j.j.lau@live.com>
2015-11-18 01:48:15 +0900

    attempt at taking all of the frames at once in init.c

--- threads/init.c
+++ threads/init.c
@@ -133,7 +133,7 @@ main (void)
   init_frametable ();
   int i = 0;
   struct frame *fs;
-  while (i < 5) {
+  while (true) {
     fs = malloc (sizeof (struct frame));
     if (fs != NULL) {
       fs->phys_addr = palloc_get_page (PAL_USER | PAL_ZERO);
