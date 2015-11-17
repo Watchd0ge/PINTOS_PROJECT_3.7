82bf716006ef8297d526b5d284d13195e85c44ae
JJ Lau <j.j.j.lau@live.com>
2015-11-17 13:08:41 +0900

    Using palloc_get_page repeatedly

--- threads/init.c
+++ threads/init.c
@@ -135,7 +135,8 @@ main (void)
   struct frame_struct *fs;
   while (i < init_ram_pages) {
     fs = malloc (sizeof (struct frame_struct));
-    &fs.vaddr = palloc_get_page (PAL_USER | PAL_ZERO);
+    fs->vaddr = palloc_get_page (PAL_USER | PAL_ZERO);
+    printf ("fs->vaddr = %p\n", fs->vaddr);
     i++;
   }
   printf ("WE HAVE %d MANY PAGES\n", i);
