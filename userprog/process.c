fab55334cb11be3ab5b60af9ddd9a71ec0ad34ad
JJ Lau <j.j.j.lau@live.com>
2015-11-17 10:16:57 +0900

    Added headers to process.c

--- userprog/process.c
+++ userprog/process.c
@@ -17,6 +17,8 @@
 #include "threads/palloc.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
+#include "vm/frame.h"
+#include "vm/swap.h"
 
 static thread_func start_process NO_RETURN;
 static bool load (const char *cmdline, void (**eip) (void), void **esp);
