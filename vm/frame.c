952198768ac79ec030088c3e73646ee6a4d57679
JJ Lau <j.j.j.lau@live.com>
2015-11-17 20:11:56 +0900

    created code for allocating a frame.

--- vm/frame.c
+++ vm/frame.c
@@ -43,10 +43,31 @@ store_frame (struct frame *f) {
   list_push_back (&frame_list, &f->elem);
 }
 
-// void
-// allocate_frame (void *phys_addr, void *v_addr, struct *thread t) {
-//   /* code */
-// }
+void
+allocate_frame (void *phys_addr, void *v_addr, struct *thread t) {
+  struct list_elem * next  = list_begin (&frame_list);
+  struct frame *fs = NULL;
+
+  /* Find an available frame */
+  while (next != NULL) {
+    fs = list_entry (next, struct frame, list_elem);
+    if (fs->owner == NULL && fs->v_addr == NULL) {
+      list_remove (next);
+      break;
+    } else {
+      next = list_next (next);
+    }
+  }
+
+  /* Allocate the available frame */
+  if (next == NULL) {
+    PANIC ("THERE ARE NO FREE PAGES IN THE FRAME TABLE\n");
+  } else {
+    fs->owner = t;
+    fs->v_addr = v_addr;
+    list_push_back (&frame_list, &fs->elem);
+  }
+}
 //
 // void
 // free_frame (struct frame *f) {
