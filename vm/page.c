e42460d31befe02289211a5f57b2f556d33ec238
JJ Lau <j.j.j.lau@live.com>
2015-11-19 00:41:47 +0900

    - -

--- vm/page.c
+++ vm/page.c
@@ -7,6 +7,8 @@
 #include "devices/block.h"
 #include "filesys/inode.h"
 #include <stdbool.h>
+#include "vm/frame.h"
+#include "vm/page.h"
 
 static unsigned
 page_hash_func (const struct hash_elem *h_elem, void *aux UNUSED)
