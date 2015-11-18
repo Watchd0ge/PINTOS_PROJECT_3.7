7994b647fa3ac79c825d7f3fdba3bf840be3f5c6
JJ Lau <j.j.j.lau@live.com>
2015-11-18 22:11:10 +0900

    -

--- userprog/exception.c
+++ userprog/exception.c
@@ -149,7 +149,7 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  // PANIC ("SHIT\n");
+  PANIC ("SHIT\n");
   sys_exit(-1);
 
   /* To implement virtual memory, delete the rest of the function
